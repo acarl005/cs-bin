56e1a1f1dcf46214f72e666047637f255a625740
Andrew Carlson <acarl005@g.ucla.edu>
2015-09-09 00:02:20 -0700

    REPL has command stack

--- src/exec.js
+++ src/exec.js
@@ -5,8 +5,23 @@ module.exports = function(hasErrors) {
     $(window).on('keypress', e => {
       e.ctrlKey && e.keyCode && execute();   //execute if they press ctrl+b
     });
+
+    $('#console form').on('keydown', e => {
+      if (e.keyCode === 38) {  //up arrow key
+        commandIndex++;
+      } else if (e.keyCode === 40) {  //down arrow key
+        commandIndex--;
+      } else {
+        return 'fuck you';
+      }
+      // make sure commandIndex stays in reasonable range
+      commandIndex = Math.min(commandStack.length - 1, commandIndex);
+      commandIndex = Math.max(-1, commandIndex);
+      $('#console form input').val(commandStack[commandIndex]);
+    });
   });
 
+  setTimeout(execute, 1000);
   function execute() {
     if (hasErrors()) return alert('fix errors first');
 
@@ -18,13 +33,20 @@ module.exports = function(hasErrors) {
       eval(code);
 
       $('#console form').off('submit');
-      $('#console form').on('submit', function(e) {
+      $('#console form').on('submit', e => {
         e.preventDefault();
+        var code = $(e.target).find('input').val();
+        commandStack.unshift(code);
+        commandStack = commandStack.slice(0, 9);
+        commandIndex = -1;
         wrapLogOutput(() => {
-          var code = $(e.target).find('input').val();
-          var output = eval(code);
-          render(`=> ${output}`);
           $(e.target).find('input').val('');
+          try {
+            var output = eval(code);
+            render(`=> ${output}`);
+          } catch(err) {
+            render('<span class="error">ERROR</span>');
+          }
         });
       });
 
@@ -33,10 +55,14 @@ module.exports = function(hasErrors) {
   }
 }
 
+var commandStack = [];
+var commandIndex = -1;
+
 function render(text) {
   $('#console #output').append(`<p>${text}</p>`);
 }
 
+// executes a function in a context where all calls to console.log will render to the DOM
 function wrapLogOutput(func) {
   console.nativeLog = console.log;
   console.log = function() {
@@ -47,4 +73,8 @@ function wrapLogOutput(func) {
   }
   func();
   console.log = console.nativeLog;
-});
+}
+
+
+
+
